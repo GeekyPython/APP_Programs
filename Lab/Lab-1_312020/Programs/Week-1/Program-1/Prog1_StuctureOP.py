979a61fb3626648be15fc75c1e83e50b14ac8ec7
GeekyPython <39787733+GeekyPython@users.noreply.github.com>
2020-01-03 17:01:10 +0530

    Program-1 Added

---
+++ Lab/Lab-1_312020/Programs/Week-1/Program-1/Prog1_StuctureOP.py
@@ -0,0 +1,7 @@
+def lele(name,age):
+    name = input("Enter your name: ")
+    age = int(input("Enter your "))
+    print("Hey",name,",you will score a century in year:",2119-age)
+sname = " "
+sage = 0
+lele(sname,sage)
